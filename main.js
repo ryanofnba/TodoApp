18be7fc3b29fe0f17fed17b5af70828b0f6eef0e
Ryan Huang <ryan_huang@intuit.com>
2018-03-10 14:59:05 -0800

    init

---
+++ main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    const getStartedButton = $(".get-started-button");
+    getStartedButton.click(function() {
+        alert("Let's begin coding! YOU CAN DO IT JENNIE KIM!");
+    });
+});
